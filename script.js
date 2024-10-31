74e5ac25d812c43aaf77dca553ce41054b923874
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-31 14:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let arr= Array(100).fill(0).map((item, i) => i+1);
+//console.log(arr);
+
+arr= arr.map(num => {
+	if(num%3===0 && num%5===0) {
+		return "FizzBuzz";
+	}
+	else if(num%3===0) {
+		return "Fizz";
+	}
+	else if(num%5===0) {
+		return "Buzz";
+	}
+	return num;
+})
+
+//console.log(arr);
+
+alert(arr)
